0fa469ea80e1f23e3124a91a70ee9edb9632282b
youtpout <eddyboughioul@hotmail.fr>
2024-04-26 08:39:06 +0200

    install noir dependencies to test proof

---
+++ packages/foundry/script/prove.ts
@@ -0,0 +1,23 @@
+import { Signature, ethers, Wallet, BaseWallet, SigningKey, Contract } from "ethers";
+import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
+import { Noir } from '@noir-lang/noir_js';
+import circuit from '../target/circuits.json';
+
+
+const privateKey = process.env.DEPLOYER_PRIVATE_KEY;
+
+const backend = new BarretenbergBackend(circuit);
+const noir = new Noir(circuit, backend);
+
+console.log("noir", noir);
+
+const input = { x: 1, y: 2 };
+
+
+console.log('logs', 'Generating proof... ⌛');
+const proof = await noir.generateProof(input);
+console.log('logs', 'Generating proof... ✅');
+console.log('results', proof.proof);
+console.log('logs', 'Verifying proof... ⌛');
+const verification = await noir.verifyProof(proof);
+if (verification) console.log('logs', 'Verifying proof... ✅');
