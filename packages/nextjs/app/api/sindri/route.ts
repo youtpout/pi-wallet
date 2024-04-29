73dd5fa106881fdf672a8a9e69ec1d899a4b1295
youtpout <eddyboughioul@hotmail.fr>
2024-04-29 11:00:31 +0200

    add sindri client

--- packages/nextjs/app/api/sindri/route.ts
+++ packages/nextjs/app/api/sindri/route.ts
@@ -1,13 +1,25 @@
+import { ethers, parseEther } from "ethers";
 import { NextApiRequest } from "next";
+import { SindriClient } from 'sindri';
+
+const feeEther = parseEther("0.01");
+// dai get 18 decimals too
+const feeDai = parseEther("1");
 
 export function GET(
     req: NextApiRequest,
 ) {
-    return Response.json({ message: 'Hello from Next.js!' })
+    const relayerKey = process.env.RELAYER_PRIVATE_KEY!;
+    const wallet = new ethers.Wallet(relayerKey);
+
+    return Response.json({ relayer: wallet.address, feeEther: feeEther.toString(), feeDai: feeDai.toString() });
 }
 
-export function POST(
+export async function POST(
     req: NextApiRequest,
 ) {
-    return Response.json({ message: 'Hello from Next.js!' })
+    // Create an instance of the `SindriClient` class.
+    const client = new SindriClient({ apiKey: process.env.SINDRI_API_KEY });
+    const response = await client.proveCircuit("e1e6361a-dc7a-4c6d-8ebb-ca2c5027d6df", req.body, true);
+    return Response.json(response);
 }
